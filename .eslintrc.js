391f8b7f98047d25416d69e01c08b2cd57966da5
Angelo Cordon <angelocordon@gmail.com>
2021-04-13 16:10:51 -0700

    Add Prettier
    Fix format errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,17 @@
 module.exports = {
-	env: { node: true },
-	root: true,
-	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		tsconfigRootDir: __dirname,
-		project: ["./tsconfig.json"],
-	},
-	plugins: ["@typescript-eslint"],
-	extends: [
-		"airbnb-typescript",
-		"airbnb/hooks",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:@typescript-eslint/recommended-requiring-type-checking",
-	],
+  env: { node: true },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
 };
--- App.tsx
+++ App.tsx
@@ -1,22 +1,20 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import Home from "screens/Home";
-
-const App: React.FC = () => {
-	return (
-		<View style={styles.container}>
-			<Home />
-		</View>
-	);
-};
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Home from 'screens/Home';
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
+
+const App: React.FC = () => (
+  <View style={styles.container}>
+    <Home />
+  </View>
+);
 
 export default App;
