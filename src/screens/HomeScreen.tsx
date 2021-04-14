09a286eb828f7c3333ecb64451a29b429f1f1bf6
Angelo Cordon <angelocordon@gmail.com>
2021-04-14 09:26:47 -0700

    Set up React Navigation for HomeScreen

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+const HomeScreen: React.FC = () => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Hello World</Text>
+  </View>
+);
+
+export default HomeScreen;
